2ef872816ad5aa3642e28015d890c1e9510811c2
nitin612 <129612660+nitin612@users.noreply.github.com>
2025-05-13 22:37:35 +0530

    some issue resolved

--- frontend/src/config/BaseUrl.js
+++ frontend/src/config/BaseUrl.js
@@ -1,7 +1,7 @@
 // const baseUrl = "http://192.168.8.136:5000/api/"; //My WiFi
 // const baseUrl = "http://10.102.11.209:5000/api/"; //Uni WiFi
 
-const baseUrl = "http://192.168.18.171:3000/api/"; 
+const baseUrl = "http://192.168.1.2:3000/api/"; 
 
 // const baseUrl = "http://localhost:3000";
 
